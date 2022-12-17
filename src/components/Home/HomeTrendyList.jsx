bf6f17dcc7983b228ebe9ea968ff2fc51ecc3181
Urichcool <104870932+Urichcool@users.noreply.github.com>
2022-12-17 22:24:36 +0100

    fix

---
+++ src/components/Home/HomeTrendyList.jsx
@@ -0,0 +1,50 @@
+import {
+  HomeTrendyStyledList,
+  HomeTrendyTitle,
+  HomeSection,
+} from './HomeTrendyList.styled';
+import { useEffect, useRef, useState } from 'react';
+import { HomeTrendyItem } from './HomeTrendyItem';
+
+export const HomeTrendyList = () => {
+  const [trandingFilms, setTrandingFilms] = useState([]);
+
+  const firstUpdate = useRef(true);
+
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+    } else {
+      fetch(
+        'https://api.themoviedb.org/3/trending/movie/week?api_key=2eb857a72ef08fb512c529b6c9a18227'
+      )
+        .then(res => res.json())
+        .then(films => {
+          films.results.map(({ title, id }) => {
+            return setTrandingFilms(state => [
+              ...state,
+              ...[
+                {
+                  title: title,
+                  id: id,
+                },
+              ],
+            ]);
+          });
+        });
+    }
+  }, []);
+
+  return (
+    <HomeSection>
+      <HomeTrendyTitle>Trending today</HomeTrendyTitle>
+      <HomeTrendyStyledList>
+        {trandingFilms.map(({ id, title }) => {
+          return (
+            <HomeTrendyItem key={id} title={title} id={id}></HomeTrendyItem>
+          );
+        })}
+      </HomeTrendyStyledList>
+    </HomeSection>
+  );
+};
---
+++ src/components/Home/HomeTrendyList.styled.jsx
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+import { NavLink } from 'react-router-dom';
+
+export const HomeSection = styled.section`
+  padding-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px grey solid;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-bottom: 30px;
+  margin-top: 5px;
+`;
+
+export const HomeTrendyStyledList = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin-top: 10px;
+`
+
+export const HomeTrendyStyledListItem = styled.li`
+margin-bottom: 10px;
+&:last-child{
+  margin-bottom: 0;
+}
+`
+
+export const HomeTrendyTitle = styled.h1`
+  margin: 0;
+`;
+
+export const StyledLink = styled(NavLink)`
+  font-weight: 600;
+  font-size: 24px;
+  color: black;
+  text-decoration: none;
+
+
+
+  &:hover,
+  &:focus {
+    color: orange;
+  }
+`;
